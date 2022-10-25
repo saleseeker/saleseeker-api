3cea718e554a12f92a3811cb2516ecdf0daad640
Jen Matlock <bast.ailuros@gmail.com>
2022-10-25 10:52:49 +0200

    Added controller stubs

---
+++ saleseeker-api/saleseeker-api/Notifications/NotificationsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace saleseeker_api.Notifications
+{
+    public class NotificationsController : ControllerBase
+    {
+        // TODO:
+        // 1: Add queue integration to send a message to the notification engine to send notifications
+        // 2: Read messages from the queue to update whether notifications were sent
+    }
+}
---
+++ saleseeker-api/saleseeker-api/Webscraper/WebscraperController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace saleseeker_api.Webscraper
+{
+    public class WebscraperController : ControllerBase
+    {
+        //TODO: 
+        // 1: Add an endpoint that when hit, hits the webscraper to trigger a scrape for the items in the db
+        // 2: Add a queue reader to receive messages from the scraper and insert the data to the db
+    }
+}
